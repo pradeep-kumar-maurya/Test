6622cdc19ce27f9f412ab9897c8258db3c6c8aa5
PRADEEP KUMAR <PRADEEP KUMAR@LAPTOP-HJ805BS5>
2020-03-20 22:31:24 +0530

    Test

---
+++ Test/src/Test.java
@@ -0,0 +1,20 @@
+abstract class A{
+	abstract void m1();
+}
+abstract class B extends A{
+	abstract void m2();
+}
+interface C{
+	void m3();
+}
+public class Test implements C {
+	public static void main(String[] args) {
+	}
+
+	@Override
+	public void m3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
